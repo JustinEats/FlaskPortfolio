2316796a2801b931b93e982c09e13af163ff0a5c
Justin Z <contactjustinzamora@gmail.com>
2020-09-03 17:34:00 -0700

    fixed to have the servers run

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MPFIGBRGWISF24'
 debug = DebugToolbarExtension(app)
 
-app.route('/')
+
+@app.route('/')
 def home_page():
     return render_template('home-page.html')
 
-app.route('/about-me')
+
+@app.route('/about-me')
 def about_me_page():
     return render_template('about-me-page.html')
 
-app.route('/contact')
+
+@app.route('/contact')
 def contact_page():
     return render_template('contact-page.html')
