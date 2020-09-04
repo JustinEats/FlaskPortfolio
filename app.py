148c21ace92df402990ebbd5c52d76b01cf1bb4e
Justin Z <contactjustinzamora@gmail.com>
2020-09-03 17:04:05 -0700

    added base css,js, and homepage

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import flask, render_template, redirect, request, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MPFIGBRGWISF24'
+debug = DebugToolbarExtension(app)
+
+app.route('/')
+def home_page():
+    return render_template('home-page.html')
