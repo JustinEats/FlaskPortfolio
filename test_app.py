de5bbce2bdbf1d3f1a14a160b8539562aadc92f9
Justin Z <contactjustinzamora@gmail.com>
2020-09-06 11:04:30 -0700

    added testing/test_app.py

---
+++ test_app.py
@@ -0,0 +1,13 @@
+from app import app
+from unittest import TestCase
+
+
+class Test_App_Routes(TestCase):
+    """Test routes for app.py"""
+
+    def test_home_page(self):
+        with app.test_client() as client:
+            res = client.get('/')
+            html = res.get_data(as_text=True)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1 class="display-4">Justin Zamora</h1>', html)
