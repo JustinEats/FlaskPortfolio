d74358b9de9e3be0c5f0dce6982b27a26dc768e1
Justin Z <contactjustinzamora@gmail.com>
2020-09-07 11:07:53 -0700

    added test_contact_page

--- test_app.py
+++ test_app.py
@@ -11,3 +11,9 @@ class Test_App_Routes(TestCase):
             html = res.get_data(as_text=True)
             self.assertEqual(res.status_code, 200)
             self.assertIn('<h1 class="display-4">Justin Zamora</h1>', html)
+
+    def test_contact_page(self):
+        with app.test_client() as client:
+            res = client.get('/contact-post')
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(res.location, 'http://localhost/')
